1fdc7dc3d9fce492e5db794493fc3fef07a0d3fb
Krantuckety <nferenczhalmy@gmail.com>
2025-10-04 10:49:25 -0500

    Project Creation
    Made 2 files to be used in the tree data structure

---
+++ fileSystemNode.py
@@ -0,0 +1,6 @@
+# Node object to be used within the tree data-structure
+class FileSystemNode:
+    def __init__(self, name, node_type="directory"):
+        self.name = name
+    def addChild(self, child):
+        print(test)
